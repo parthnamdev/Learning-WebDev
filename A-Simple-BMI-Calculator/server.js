71eb767985b6dec5cd04e356ca08c548950dee88
parthnamdev <parthnamdev@users.noreply.github.com>
2020-04-29 15:08:58 +0530

    sending basic home page

---
+++ A-Simple-BMI-Calculator/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.listen(3000, function(){
+    console.log("server started at port 3000");
+});
