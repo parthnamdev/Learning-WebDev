2d8cd4c12b6c8e4d9b607a4cfb81b840dfa4cb4a
parthnamdev <parthnamdev@users.noreply.github.com>
2020-05-03 19:22:50 +0530

    signup page

--- Newsletter-Signup-Sample/app.js
+++ Newsletter-Signup-Sample/app.js
@@ -3,9 +3,18 @@ const bodyParser = require("body-parser");
 const request = require("request");
 
 const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-    res.send("hello!, world");
+    res.sendFile(__dirname + "/signup.html");
+})
+
+app.post("/", function(req, res){
+    var firstName = req.body.firstName;
+    var lastName = req.body.lastName;
+    var email = req.body.email;
+    console.log(firstName, lastName, email);
 })
 
 app.listen(3000, function(){
