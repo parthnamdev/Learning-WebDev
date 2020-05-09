e48a06d7c003f8188c1c4a5ff6737e583d5521a9
parthnamdev <parthnamdev@users.noreply.github.com>
2020-05-09 15:54:29 +0530

    major change

---
+++ to-do-list/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+    var today = new Date();
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+    var day = today.toLocaleDateString("en-US", options);
+    res.render("list", { kindOfDay: day });
+});
+
+app.listen("3000", function(){
+   console.log("Server started at port 3000");
+});
+console.log('hello')
