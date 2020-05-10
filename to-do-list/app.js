e144002b59eb7c31f2b0d81897af86cace98f84a
parthnamdev <parthnamdev@users.noreply.github.com>
2020-05-10 19:18:33 +0530

    basic ejs template

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+var items = ["Assignment", "Project", "Quiz"];
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
     var today = new Date();
@@ -13,10 +14,13 @@ app.get("/", function(req, res){
         month: "long"
     };
     var day = today.toLocaleDateString("en-US", options);
-    res.render("list", { kindOfDay: day });
+    res.render("list", { kindOfDay: day, newListItems: items });
 });
-
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 app.listen("3000", function(){
    console.log("Server started at port 3000");
 });
-console.log('hello')
