cdde9cfcef05a510038611a1b1e1e06a78219de7
parthnamdev <parthnamdev@users.noreply.github.com>
2020-06-14 18:41:05 +0530

    simple encryption decryption

--- simplest-Authentication/app.js
+++ simplest-Authentication/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const e = require("express");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -12,10 +12,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+
+const secret = "thisisanencryptionstring.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
